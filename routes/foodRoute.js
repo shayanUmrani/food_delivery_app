a525fe5c54ac27dd3e09fa30ddefb599341956c0
shayanUmrani <shayan.umrani.acemedassist@gmail.com>
2025-11-14 10:50:31 +0500

    added

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -7,15 +7,18 @@ const foodRouter = express.Router();
 // Image Storage Engine
 
 const storage = multer.diskStorage({
-    destination:"uploads",
-    filename: (req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
+    destination: (req, file, cb) => {
+        cb(null, "/tmp");   // <-- Bas ye line change ki gayi hai
+    },
+    filename: (req, file, cb) => {
+        return cb(null, `${Date.now()}${file.originalname}`);
     }
-})
+});
 
-const upload = multer({storage:storage})
-foodRouter.post('/add',upload.single('image'),addFood)
-foodRouter.get('/list',listFood)
-foodRouter.post('/remove', removeFood)
+const upload = multer({ storage: storage });
+
+foodRouter.post('/add', upload.single('image'), addFood);
+foodRouter.get('/list', listFood);
+foodRouter.post('/remove', removeFood);
 
 export default foodRouter;
