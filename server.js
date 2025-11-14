a68a52636ebfcddf1482011416ed08b1aedf0511
shayanUmrani <shayan.umrani.acemedassist@gmail.com>
2025-11-14 10:32:39 +0500

    changes in server.js

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const port = 4000
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000","https://mern-food-delivery-app-frontend.vercel.app","https://mern-food-delivery-app-admin.vercel.app"],
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true
+}))
 
 //db connection
 connectDB();
